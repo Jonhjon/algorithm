b8a0503d1d4e33c9bd6fbda2c4fabcdb513703b2
Jonhjon <86086178+Jonhjon@users.noreply.github.com>
2024-09-18 17:20:00 +0800

    Update Source.cpp

--- 09-18-上機/Source.cpp
+++ 09-18-上機/Source.cpp
@@ -4,23 +4,22 @@ using namespace std;
 
 void test_fun(int A, int B, int result) {
 	int check = 0;
-	if (A > 1)
+	if (A != 0)
 	{
 		A = 1;
 	}
-	if (B > 1)
+	if (B != 0)
 	{
 		B = 1;
-	}
-
-	if ((A | B) == result)
-	{
-		cout << "OR\n";
-		check = 1;
 	}
 	if ((A & B) == result)
 	{
 		cout << "AND\n";
+		check = 1;
+	}
+	if ((A | B) == result)
+	{
+		cout << "OR\n";
 		check = 1;
 	}
 	if ((A ^ B) == result)
@@ -45,7 +44,20 @@ int main() {
 		//test(A, B, result);
 		cout << "case " << i + 1 << " : " << test[i][0] << " " << test[i][1] << " " << test[i][2] << " \n";
 		test_fun(test[i][0], test[i][1], test[i][2]);
+		cout << endl;
 	}
-	
+	while (true)
+	{
+		cin >> A >> B >> result;
+		if ((result != 0) && result != 1)
+		{
+			cout << "result erro 只有 0 或 1\n";
+		}
+		else {
+			test_fun(A, B, result);
+			break;
+		}
+	}
+
 	return 0;
 }
