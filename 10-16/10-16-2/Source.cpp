f4fb6ddb0f387d62b6f8c8e7d68afe876db0373b
Jonhjon <86086178+Jonhjon@users.noreply.github.com>
2024-10-16 18:11:47 +0800

    Update Source.cpp

--- 10-16/10-16-2/Source.cpp
+++ 10-16/10-16-2/Source.cpp
@@ -1,10 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <limits.h>
+#include<time.h>
 using namespace std;
 
-int** Read_File(string path,int &n) {
+int** Read_File(string path, int& n) {
     fstream file;
     //int n = 0;
     file.open(path);
@@ -31,7 +32,7 @@ int** Read_File(string path,int &n) {
     file.close();
     return A;
 }
-void Printf_matrix(int** A,int n) {
+void Printf_matrix(int** A, int n) {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -43,12 +44,12 @@ void Printf_matrix(int** A,int n) {
     }
     cout << endl;
 }
-void Free(int** data,int n) {
+void Free(int** data, int n) {
     for (int i = 0; i < n; i++)
         delete[] data[i];
     delete[] data;
 }
-int**creat_memory(int n) {
+int** creat_memory(int n) {
     int** C = new  int* [n];
     for (int i = 0; i < n; i++)
     {
@@ -56,67 +57,12 @@ int**creat_memory(int n) {
     }
     return C;
 }
-void matrix_add(int n, int **A, int **B, int **C) {
+void matrix_add(int n, int** A, int** B, int** C) {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             C[i][j] = A[i][j] + B[i][j];
 }
-//void matrix_multiply_recursive(int n, int** A, int** B, int** C) {
-//    if (n == 1) {
-//        C[0][0] = A[0][0] * B[0][0];
-//        return;
-//    }
-//
-//    int mid = n / 2;
-//    int** A11 = creat_memory(mid),**A12 = creat_memory(mid), **A21 = creat_memory(mid), **A22 = creat_memory(mid);
-//    int** B11 = creat_memory(mid), **B12 = creat_memory(mid), **B21 = creat_memory(mid), **B22 = creat_memory(mid);
-//    int** C11 = creat_memory(mid), **C12 = creat_memory(mid), **C21 = creat_memory(mid), **C22 = creat_memory(mid);
-//    int** temp = creat_memory(mid);
-//    // 分割矩阵
-//    for (int i = 0; i < mid; i++) {
-//        for (int j = 0; j < mid; j++) {
-//            A11[i][j] = A[i][j];
-//            A12[i][j] = A[i][j + mid];
-//            A21[i][j] = A[i + mid][j];
-//            A22[i][j] = A[i + mid][j + mid];
-//
-//            B11[i][j] = B[i][j];
-//            B12[i][j] = B[i][j + mid];
-//            B21[i][j] = B[i + mid][j];
-//            B22[i][j] = B[i + mid][j + mid];
-//        }
-//    }
-//
-//    // 递归计算
-//    matrix_multiply_recursive(mid, A11, B11, C11);
-//    matrix_multiply_recursive(mid, A12, B21, temp);
-//    matrix_add(mid, C11, temp, C11);
-//
-//    matrix_multiply_recursive(mid, A11, B12, C12);
-//    matrix_multiply_recursive(mid, A12, B22, temp);
-//    matrix_add(mid, C12, temp, C12);
-//
-//    matrix_multiply_recursive(mid, A21, B11, C21);
-//    matrix_multiply_recursive(mid, A22, B21, temp);
-//    matrix_add(mid, C21, temp, C21);
-//
-//    matrix_multiply_recursive(mid, A21, B12, C22);
-//    matrix_multiply_recursive(mid, A22, B22, temp);
-//    matrix_add(mid, C22, temp, C22);
-//    // 组合结果
-//    for (int i = 0; i < mid; i++) {
-//        for (int j = 0; j < mid; j++) {
-//            C[i][j] = C11[i][j];
-//            C[i][j + mid] = C12[i][j];
-//            C[i + mid][j] = C21[i][j];
-//            C[i + mid][j + mid] = C22[i][j];
-//        }
-//    }
-//    Free(A11,mid);Free(A12, mid);Free(A21, mid);Free(A22, mid);
-//    Free(B11, mid);Free(B12, mid);Free(B21, mid);Free(B22, mid); 
-//    Free(C11, mid);Free(C12, mid);Free(C21, mid);Free(C22, mid);
-//    Free(temp, mid);
-//}
+
 void matrix_subtract(int n, int** A, int** B, int** C) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -125,22 +71,20 @@ void matrix_subtract(int n, int** A, int** B, int** C) {
     }
 }
 
-// 使用 Strassen 算法進行矩陣乘法
-// 將矩陣 A 和 B 相乘，結果儲存在矩陣 C 中
 void strassen_matrix_multiply(int n, int** A, int** B, int** C) {
-    // 基本情況：當矩陣大小為 1x1 時，直接相乘
+    
     if (n == 1) {
         C[0][0] = A[0][0] * B[0][0];
     }
     else {
-        int mid = n / 2;  // 將矩陣劃分為 2x2 的子矩陣
+        int mid = n / 2;  
 
         // 分配子矩陣
-        int** A11 = creat_memory(mid), ** A12 = creat_memory(mid), **A21 = creat_memory(mid),** A22 = creat_memory(mid);
-        int** B11 = creat_memory(mid), **B12 = creat_memory(mid), **B21 = creat_memory(mid), **B22 = creat_memory(mid);
-        int** C11 = creat_memory(mid), **C12 = creat_memory(mid), **C21 = creat_memory(mid), **C22 = creat_memory(mid);
-        int **M1 = creat_memory(mid), **M2 = creat_memory(mid), **M3 = creat_memory(mid), **M4 = creat_memory(mid),**M5 = creat_memory(mid), **M6 = creat_memory(mid), **M7 = creat_memory(mid);
-        int **temp1 = creat_memory(mid), **temp2 = creat_memory(mid);  // 用於中間結果的矩陣
+        int** A11 = creat_memory(mid), ** A12 = creat_memory(mid), ** A21 = creat_memory(mid), ** A22 = creat_memory(mid);
+        int** B11 = creat_memory(mid), ** B12 = creat_memory(mid), ** B21 = creat_memory(mid), ** B22 = creat_memory(mid);
+        int** C11 = creat_memory(mid), ** C12 = creat_memory(mid), ** C21 = creat_memory(mid), ** C22 = creat_memory(mid);
+        int** M1 = creat_memory(mid), ** M2 = creat_memory(mid), ** M3 = creat_memory(mid), ** M4 = creat_memory(mid), ** M5 = creat_memory(mid), ** M6 = creat_memory(mid), ** M7 = creat_memory(mid);
+        int** temp1 = creat_memory(mid), ** temp2 = creat_memory(mid);  // 用於中間結果的矩陣
 
         // 初始化結果矩陣 C 的子矩陣為 0
         for (int i = 0; i < mid; i++) {
@@ -219,24 +163,32 @@ void strassen_matrix_multiply(int n, int** A, int** B, int** C) {
         Free(temp1, mid); Free(temp2, mid);
     }
 }
+
 int main() {
-   
+
     fstream file;
-    string path = "Data/2A.txt";
-    string path2 = "Data/2B.txt";
+    string path = "Data/1A.txt";
+    string path2 = "Data/1B.txt";
     int n = 0, n2 = 0;
-    int** A=NULL, ** B =NULL, ** C=NULL;
-    
-    A = Read_File(path , n );
+    clock_t start_t= 0, finish_t = 0;
+
+    int** A = NULL, ** B = NULL, ** C = NULL;
+
+    A = Read_File(path, n);
     Printf_matrix(A, n);
     B = Read_File(path2, n2);
     Printf_matrix(B, n2);
     C = creat_memory(n);
-    
-    strassen_matrix_multiply(n, A, B, C);
-    
+    start_t = clock();
+    for (int i = 0; i < 10000; i++)
+    {
+        strassen_matrix_multiply(n, A, B, C);
+        finish_t = clock();
+    }
     Printf_matrix(C, n);
-
+    double Time = (double)(finish_t - start_t) / CLOCKS_PER_SEC;
+    cout << "時間為 : " << Time;
+    //cout << start_t<<"  "<<finish_t;
     Free(A, n);
     Free(B, n2);
     Free(C, n);
